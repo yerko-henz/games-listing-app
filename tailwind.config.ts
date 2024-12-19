0239ef90e6ba5607b5d11257f3ac59adc9808c02
Yerko Henriquez R <yerko.henz@gmail.com>
2024-12-19 17:47:20 -0300

    feat: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        page: {
+          bgPrimary: "#EFEDF3",
+          bgSecondary: "#EEEEEE",
+          bgTertiary: "#404040",
+        },
+        text: {
+          primary: "#3B3B3B",
+          secondary: "#737373",
+        },
+        border: {
+          primary: "#8F8F8F",
+          secondary: "#3B3B3B",
+        },
+        button: {
+          bgPrimary: "#585660",
+          bgSecondary: "#FFFFFF",
+        },
+      },
+      screens: {
+        xs: "320px",
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
